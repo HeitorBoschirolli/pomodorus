f882cecfd2b3a3f16b3a95976244af7cb1cbec8f
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-02-06 19:58:59 -0300

    Create project template

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+/**
+ * IMPORTS
+ */
+import React from 'react';
+
+
+/**
+ * CODE
+ */
+class App extends React.Component
+{
+    /**
+     * I render the application.
+     *
+     * :returns: the content do be rendered
+     * :rtype: React.ReactNode
+     */
+    render ()
+    {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+
+/**
+ * EXPORTS
+ */
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+/**
+ * IMPORTS
+ */
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+/**
+ * CODE
+ */
+ReactDOM.render(<App />, document.querySelector('#root'));
