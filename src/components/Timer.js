0bfe109e76827c0a63b79e7a029e36ad25e9a1eb
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-02-19 18:36:03 -0300

    Create timer component

---
+++ src/components/Timer.js
@@ -0,0 +1,99 @@
+/**
+ * IMPORTS
+ */
+import React from 'react';
+
+
+/**
+ * CODE
+ */
+class Timer extends React.Component
+{
+    /**
+     * I format the timer values to a human-readable format with two digits for
+     * hours, minutes and seconds.
+     */
+    formatTime ()
+    {
+        const hours = ('0' + this.props.hours).slice(-2);
+        const minutes = ('0' + this.props.minutes).slice(-2);
+        const seconds = ('0' + this.props.seconds).slice(-2);
+
+        return `${hours}:${minutes}:${seconds}`;
+    }
+
+
+    /**
+     * I handle the 'start' button click.
+     */
+    onStartClick ()
+    {
+        this.props.onStartClick();
+    }
+
+
+    /**
+     * I handle the 'stop' button click.
+     */
+    onStopClick ()
+    {
+        this.props.onStopClick();
+    }
+
+
+    /**
+     * I handle the 'reset' button click.
+     */
+    onResetClick ()
+    {
+        this.props.onResetClick();
+    }
+
+
+    /**
+     * I render this component
+     */
+    render()
+    {
+        return (
+            <div className="container text-center">
+                <div className="display-3 m-5">
+                    {this.formatTime()}
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="btn-group col-sm-12">
+                            <button
+                                type="button"
+                                className="btn btn-secondary col-md-4"
+                                onClick={() => {this.onStartClick()}}
+                            >
+                                Start
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-secondary col-md-4"
+                                onClick={() => {this.onStopClick()}}
+                            >
+                                Stop
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-secondary col-md-4"
+                                onClick={() => {this.onResetClick()}}
+                            >
+                                Reset
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+/**
+ * EXPORTS
+ */
+export default Timer;
