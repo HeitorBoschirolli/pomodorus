7c282e32835ad7ef1c5dfa72c473ecdc8c42bb47
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-02-19 19:10:47 -0300

    Updates

---
+++ precache-manifest.4b99fe4d1ae7e95306e0071401c023dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c85f04d7add335b133ef0f5effaa10d",
+    "url": "/pomodorus/index.html"
+  },
+  {
+    "revision": "556805c43cfd695a9c42",
+    "url": "/pomodorus/static/js/2.7bb212c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodorus/static/js/2.7bb212c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad003ab0b54177cd7969",
+    "url": "/pomodorus/static/js/main.f34f9642.chunk.js"
+  },
+  {
+    "revision": "f59532ef27ef5986080a",
+    "url": "/pomodorus/static/js/runtime-main.c4f155aa.js"
+  },
+  {
+    "revision": "0be7d612ecd3ca735626c4dbfe564682",
+    "url": "/pomodorus/static/media/352666__foolboymedia__up-chime-2.0be7d612.mp3"
+  }
+]);
